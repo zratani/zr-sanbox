ff5620423fe474c772ac560d798502b4f126b519
zratani <90156231+zratani@users.noreply.github.com>
2021-09-14 14:47:47 -0400

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "instance_type" {
+  description = "Type of EC2 instance to provision"
+  default     = "t2.micro"
+}
+
+variable "instance_name" {
+  description = "EC2 instance name"
+  default     = "Provisioned by tf-zratani0123"
+}
+
+variable "env_name" {
+  description = "Environment"
+  default     = "PROD"
+}
+
+
